4f2b76187ae0544f21eb8efda7cb2e33ac64bc8a
Igor Shevchenko <cyberskunk@gmail.com>
2018-03-15 11:37:26 +0300

    chore: add eslint with prettier and lint sources

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const tsm = require('teamcity-service-messages')
 
-exports.success = () => 'success';
-exports.error = (error, args) =>  console.error(error,
-  args.verbose && JSON.stringify(args) || '');
+exports.success = () => 'success'
+exports.error = (error, args) => console.error(error, (args.verbose && JSON.stringify(args)) || '')
 
-exports.check = {};
-exports.check.success = (result, args) => {
-  const nspInspectionTypeId = 'nsp-security-inspection';
+exports.check = {}
+exports.check.success = result => {
+  const nspInspectionTypeId = 'nsp-security-inspection'
 
   if (result.data.length) {
     tsm.inspectionType({
       id: nspInspectionTypeId,
       name: 'NSP security inspection',
       category: 'security',
-      description: 'https://www.npmjs.com/package/nsp'
+      description: 'https://www.npmjs.com/package/nsp',
     })
     result.data.forEach(elem => {
       tsm.inspection({
@@ -25,8 +24,8 @@ patched_versions: ${elem.patched_versions},
 recommendation: ${elem.recommendation},
 advisory: ${elem.advisory}`,
         file: `module: "${elem.module}"`,
-        SEVERITY: 'WARNING'
+        SEVERITY: 'WARNING',
       })
-    });
+    })
   }
 }

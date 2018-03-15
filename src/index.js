5e9d0e891c9ddf401a92c2f6eabd08c0d60358b5
Igor Shevchenko <cyberskunk@gmail.com>
2018-03-15 12:27:23 +0300

    refactor: split reporter and teamcity-service-messages for testability

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 const tsm = require('teamcity-service-messages')
 
-exports.success = () => 'success'
-exports.error = (error, args) => console.error(error, (args.verbose && JSON.stringify(args)) || '')
+const {successHandler, errorHandler, createCheckSuccessHandler} = require('../lib/reporter')
+
+exports.success = successHandler
+exports.error = errorHandler
 
 exports.check = {}
-exports.check.success = result => {
-  const nspInspectionTypeId = 'nsp-security-inspection'
-
-  if (result.data.length) {
-    tsm.inspectionType({
-      id: nspInspectionTypeId,
-      name: 'NSP security inspection',
-      category: 'security',
-      description: 'https://www.npmjs.com/package/nsp',
-    })
-    result.data.forEach(elem => {
-      tsm.inspection({
-        typeId: nspInspectionTypeId,
-        message: `${elem.overview}
-version: ${elem.version},
-vulnerable_versions: ${elem.vulnerable_versions},
-patched_versions: ${elem.patched_versions},
-recommendation: ${elem.recommendation},
-advisory: ${elem.advisory}`,
-        file: `module: "${elem.module}"`,
-        SEVERITY: 'WARNING',
-      })
-    })
-  }
-}
+exports.check.success = createCheckSuccessHandler(tsm)

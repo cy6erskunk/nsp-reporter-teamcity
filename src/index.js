5a849bdbe69027c21a32ba436946f48d3ead03bc
cyberskunk <cyberskunk@gmail.com>
2018-03-25 22:02:22 +0300

    chore: prepare reporter to become configurable

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const tsm = require('teamcity-service-messages')
 
+const config = require('../lib/config')
 const {successHandler, errorHandler, createCheckSuccessHandler} = require('../lib/reporter')
 
 exports.success = successHandler
 exports.error = errorHandler
 
 exports.check = {}
-exports.check.success = createCheckSuccessHandler(tsm)
+exports.check.success = createCheckSuccessHandler(tsm, config)
